c8f4eec205bb7e23cd9c24c49989cd76e816efa2
albilug <alberto.lugato@gmail.com>
2023-06-22 12:16:58 +0200

    Committo tutto

--- initial/src/main/java/com/example/springboot/Exercise5/DAO/IngredientDao.java
+++
@@ -1,6 +0,0 @@
-package com.example.springboot.Exercise5.DAO;
-
-import com.example.springboot.Exercise5.Model.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientDao extends JpaRepository<Ingredient, Long> {}
